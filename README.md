41be5e3f5de354d8846196263425dabff684bd11
Jeffri Sandy <jeffri.sandy@gmail.com>
2019-03-29 22:26:34 +0700

    Final Submission

--- README.md
+++ README.md
@@ -1,22 +1,20 @@
 # Basketball Team Stats Tool
-In this project you will be writing a program that reads from the "constants" data (`PLAYERS` and `TEAMS`) in `constants.py`. This data will need to be translated into a new collection of your choosing and the fields need to be changed to something that makes more sense for Python to do its comparisons.
+Python Web Development TechDegree **Project 2 - Basketball Team Stats Tool**
 
+## Description
 
-**NOTE**: Python has no concept of actual constants like some other languages out there. But it is a convention in Python to treat ALL CAPS variables as if they are in-fact constants.
+In this project you will be writing a program that reads from the "constants" data (`PLAYERS` and `TEAMS`) in `constants.py`. This data will need to be translated into a new data structure to store and organize a team of Basketball players into distributed teams. This tool will not only balance the teams by the total number of players but also let you generate some statistics for a given team.
 
+<img src="src/app.png">
 
-**Steps to get started:**
+## Instructions:
 
-1. Create a new empty script file called `app.py` or `application.py`
+run `python app.py`
 
-2. Inside this new file, you will want a Dunder Main statement:
-   For a refresh on Dunder Main:
-   https://teamtreehouse.com/library/understanding-dunder-main-main
+Note: you should have Python 3.6 or later installed in your computer.
 
-3. Any print statements or function calls you will want to be inside Dunder Main or inside a main function call which is nested inside Dunder Main.
-   If you need a refresh, check out the supplied Project 1 files/workspace for an example.
-   
-
-
-If you get stuck, try to work through the problem. Sometimes it helps to try to write/draw out your steps on paper in the order your program should run in and solve each step 1 at a time. If you are still stuck be sure to reach out in the Python Techdegree #unit-02 Slack channel.
-
+## File Description
+There are 3 function available :
+- `app.py` : as main function to run the app.
+- `constants.py` : to store player data.
+- `helper.py` : as utilities function and also to clean player data.
--- helper.py
+++ helper.py
@@ -1,6 +1,5 @@
 from constants import TEAMS, PLAYERS
 
-
 #####################
 # CLEANING FUNCTION #
 #####################
@@ -23,18 +22,18 @@ def clean_guardians(str):
 
 def clean_player(player):
     """ This function is to clean player data  """
-    
+
     player_dict = {}
     player_dict["name"] = player['name']
     player_dict["experience"] = clean_experience(player["experience"])
     player_dict["height"] = clean_height(player["height"])
     player_dict["guardians"] = clean_guardians(player["guardians"])
-    
+
     return player_dict
 
 def clean_player_list():
     """ This function is to clean all player data in PLAYERS """
-    
+
     clean_players = []
 
     for player in PLAYERS:
@@ -53,66 +52,66 @@ def display_menu_option():
     print("Here are your choices:")
     print("1) Display Team Stats")
     print("2) Quit\n")
-    
+
 def display_team_option(team_names):
     """ this function is to display team names """
-    
+
     print("\nTeams : ")
     for i, name in enumerate(team_names):
         print(f"{i+1}) {name}")
 
 def display_total_players(num_player, num_exp_player, num_not_exp_player):
     """ this function is to display total players, number of experience and inexperience players """
-    
+
     print(f"Total_players: {num_player}")
     print(f"> Experience players : {num_exp_player}")
     print(f"> Inexperience players : {num_not_exp_player}")
-    
+
 def display_player_name(team):
     """ this function is to display player name in the team """
-    
+
     print("\nPlayers on Team:")
     print(f"> {', '.join(team)}")
 
 def display_height(avg_height):
     """ this function is to display average player height in the team"""
-    
+
     print(f"\nTeam Average Height (inches) : {avg_height:.2f}")
-    
+
 def display_guardians(guardian_list):
     """ this function is to display player guadian names """
-    
+
     print("\nPlayer Guardians :")
     print(f"> {', '.join(guardian_list)}")
-    
-def display_stats(team, team_str, num_player, 
-                  num_exp_player, num_not_exp_player, 
+
+def display_stats(team, team_str, num_player,
+                  num_exp_player, num_not_exp_player,
                   avg_height, guardian_list):
     """
     This function is to display team statistics
     """
-    
+
     print("\n" + team_str)
     print("-" * len(team_str))
-    
+
     # print total players
     display_total_players(num_player, num_exp_player, num_not_exp_player)
-                      
+
     # print players name
-    display_player_name(team)     
-    
+    display_player_name(team)
+
     # print average player height
-    display_height(avg_height)          
-    
+    display_height(avg_height)
+
     # print put player guardian names
     display_guardians(guardian_list)
-    
+
 
 #######################
 # MISC FUNCTION       #
 #######################
-    
-def get_input(min_val, max_val): 
+
+def get_input(min_val, max_val):
     """ This function is to get input from user, and check the input for int and within range """
 
     while True:
@@ -126,23 +125,23 @@ def get_input(min_val, max_val):
             print("The value entered was not a number, try again.")
         else:
             return option
-        
+
 def get_experience_players(player_list):
     """ this function is to get a list of experience player name in player_list (cleaned PLAYERS) """
-    
+
     return [player['name']  for player in player_list  if player['experience']]
 
 def get_not_experience_players(player_list):
     """ this function is to get a list of inexperience player name in player_list (cleaned PLAYERS) """
-    
+
     return [player['name']  for player in player_list  if not player['experience']]
 
 def balance_players(players):
-    """ 
+    """
     this function is to balance and divide list of players into 3 team
     It returns a list of player list, 3 teams x n players
     """
-    
+
     n = int(len(players) / 3)
     bal_players = [players[i:i + n] for i in range(0, len(players), n)]
     return bal_players
@@ -152,20 +151,20 @@ def create_team(exp_players, not_exp_players):
     This function is to crete a balance team within experience and inexperience team
     It returns a list of player list, 3 teams x n players
     """
-    
+
     # get a list of player names : experience and inexperience
     bal_exp_players = balance_players(exp_players)
     bal_not_exp_players = balance_players(not_exp_players)
-    
+
     #create an empty list to store a list of player list
     teams = []
-    
+
     for i in range(len(bal_exp_players)):
         team = bal_exp_players[i] + bal_not_exp_players[i]
         teams.append(team)
-    
+
     return teams
-  
+
 def get_height_list(name_list, player_list):
     """
     This function is to get the list of player height
@@ -175,12 +174,12 @@ def get_height_list(name_list, player_list):
     RETURN:
         a list of player height given a player name_list
     """
-        
+
     return [player['height'] for player in player_list if player["name"] in name_list]
 
-def average(lst): 
+def average(lst):
     """ This function to calculate average of given list """
-    return sum(lst) / len(lst) 
+    return sum(lst) / len(lst)
 
 def get_num_exp_players(name_list, player_list):
     """
@@ -191,7 +190,7 @@ def get_num_exp_players(name_list, player_list):
     RETURN:
         int, number of experience player given the player name_list
     """
-        
+
     exp_arr = [int(player["experience"]) for player in player_list if player['name'] in name_list]
     return sum(exp_arr)
 
@@ -204,12 +203,12 @@ def get_guardians(name_list, player_list):
     RETURN:
         a list of player guardian name given a player name_list
     """
-    
+
     return [guardian for player in player_list for guardian in player["guardians"] if player["name"] in name_list]
 
 def get_team_stats(team, team_str, clean_players):
     """
-    INPUT : 
+    INPUT :
         team : list of player names
         team_str : a string to display name of the team
         clean_players : list of cleaned PLAYERS
@@ -222,11 +221,11 @@ def get_team_stats(team, team_str, clean_players):
         avg_height : float, average height players
         guardian_list : list of player guardians
     """
-    
+
     num_player = len(team)
     num_exp_player = get_num_exp_players(team, clean_players)
     num_not_exp_player = num_player - num_exp_player
-    
+
     height_list = get_height_list(team, clean_players)
     avg_height = average(height_list)
     guardian_list = get_guardians(team, clean_players)
